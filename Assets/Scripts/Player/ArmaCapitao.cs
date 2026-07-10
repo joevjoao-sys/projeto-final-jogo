5fd588f238167e4fd12a2ae5b309f6dc9956360f
joevjoao-sys <joevjoao@gmail.com>
2026-07-09 22:00:49 -0300

    Update ArmaCapitao.cs

--- Assets/Scripts/Player/ArmaCapitao.cs
+++ Assets/Scripts/Player/ArmaCapitao.cs
@@ -8,14 +8,14 @@ public class ArmaCapitao : MonoBehaviour
     [Header("Configurações de Combate")]
     public float dano = 25f;
     public float alcance = 50f;
-    public float tempoEntreTiros = 0.1f;
+    public float tempoEntreTiros = 0;
     private float danoOriginal; 
 
     [Header("Munição")]
     public int capacidadeDoPente = 8;
     private int municaoNoPente;
     public int balasNaReserva = 24;
-    public float tempoDeRecarga = 1.5f; 
+    public float tempoDeRecarga = 1; 
     private bool isReloading = false; 
 
     [Header("Interface (UI)")]
